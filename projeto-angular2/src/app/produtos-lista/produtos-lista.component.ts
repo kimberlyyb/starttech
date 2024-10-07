960261853598b9eaf9b3564e158ab5115c817c2f
Kimberly <kimberlyybarros@gmail.com>
2024-10-07 12:36:26 -0300

    adicionei a logica

--- projeto-angular2/src/app/produtos-lista/produtos-lista.component.ts
+++ projeto-angular2/src/app/produtos-lista/produtos-lista.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Produto } from '../models/produtos.models';
+import { ProdutoService } from '../produto.service';
 
 @Component({
   selector: 'app-produtos-lista',
   standalone: true,
-  imports: [],
+  imports: [RouterModule,CommonModule],
   templateUrl: './produtos-lista.component.html',
   styleUrl: './produtos-lista.component.css'
 })
-export class ProdutosListaComponent {
+export class ProdutosListaComponent implements OnInit{
+produtos: Produto[] = []
+
+constructor(private produtoService: ProdutoService) {}
+
+ngOnInit(): void{
+  this.produtos = this.produtoService.getProdutos();
+}
+deletarProduto(id:number): void{
+  this.produtoService.deleteProduto(id);
+  this.produtos = this.produtoService.getProdutos();
+}
 
 }
